b8d3548f746ef3a876de216586326a49f7935d17
surya1337gkrm <vijjanas1@udayton.edu>
2023-01-24 18:19:05 -0500

    Fixed for Multiple Cards

--- assignment02/cards.go
+++ assignment02/cards.go
@@ -1,49 +1,19 @@
+package main
+
 //Code to display cards based on the command line arguments
 //Author: Surya Venkatesh Vijjana
 //Course: CPS544
-
-package main
-
 import (
-	"fmt"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
+    "fmt"
+    "math/rand"
+    "os"
+    "strings"
+    "time"
 )
 
-var card1 string
-var card2 string
-
-// Function to assign values to the card1 and card2
-func cards(arr [13]string, s map[string]string) {
-	if os.Args[2] == "r" {
-		card1 = arr[rand.Intn(len(arr))]
-	} else {
-		_, keyFound := s[os.Args[2]]
-		if keyFound {
-			card1 = os.Args[2]
-		} else {
-			fmt.Println("Invalid Argument for Card1.Choose from 2-10/a,k,q,j or r.")
-			return
-		}
-	}
-	if os.Args[3] == "r" {
-		card2 = arr[rand.Intn(len(arr))]
-	} else {
-		_, keyFound := s[os.Args[3]]
-		if keyFound {
-			card2 = os.Args[3]
-		} else {
-			fmt.Println("Invalid Argument for card2.Choose from 2-10/a,k,q,j or r.")
-			return
-		}
-	}
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	a := `----------------
+    rand.Seed(time.Now().UnixNano())
+    a := `----------------
 |              |
 |      /\      |
 |     /  \     |
@@ -54,7 +24,7 @@ func main() {
 |              |
 ----------------
 `
-	j := `----------------
+    j := `----------------
 |              |
 |  ----------  |
 |       |      |
@@ -65,7 +35,7 @@ func main() {
 |              |
 ----------------
 `
-	q := `----------------
+    q := `----------------
 |              |
 |  ----------  |
 |  |        |  |
@@ -77,7 +47,7 @@ func main() {
 ----------------
 `
 
-	k := `----------------
+    k := `----------------
 |              |
 |     |  /     |
 |     | /      |
@@ -88,7 +58,7 @@ func main() {
 |              |
 ----------------
 `
-	char2 := `----------------
+    char2 := `----------------
 |    ________  |
 |           |  |
 |           |  |
@@ -99,7 +69,7 @@ func main() {
 |              |
 ----------------
 `
-	char3 := `----------------
+    char3 := `----------------
 |   ________   |
 |          |   |
 |          |   |
@@ -110,7 +80,8 @@ func main() {
 |              |
 ----------------
 `
-	char4 := `----------------
+    char4 := `----------------
+|              |
 |       /|     |
 |      / |     |
 |     /  |     |
@@ -118,10 +89,9 @@ func main() {
 |   /____|__   |
 |        |     |
 |        |     |
-|              |
 ----------------
 `
-	char5 := `----------------
+    char5 := `----------------
 |   ________   |
 |   |          |
 |   |          |
@@ -132,7 +102,7 @@ func main() {
 |              |
 ----------------
 `
-	char6 := `----------------
+    char6 := `----------------
 |   ________   |
 |   |          |
 |   |          |
@@ -143,7 +113,7 @@ func main() {
 |              |
 ----------------
 `
-	char7 := `----------------
+    char7 := `----------------
 |   ________   |
 |          /   |
 |         /    |
@@ -154,7 +124,7 @@ func main() {
 |              |
 ----------------
 `
-	char8 := `----------------
+    char8 := `----------------
 |   ________   |
 |   |       |  |
 |   |       |  |
@@ -165,7 +135,7 @@ func main() {
 |              |
 ----------------
 `
-	char9 := `----------------
+    char9 := `----------------
 |   ________   |
 |  |       |   |
 |  |       |   |
@@ -176,7 +146,7 @@ func main() {
 |              |
 ----------------
 `
-	char10 := `----------------
+    char10 := `----------------
 |              |
 |  |  -------  |
 |  |  |     |  |
@@ -188,40 +158,68 @@ func main() {
 ----------------
 `
 
-	stringMap := map[string]string{
-		"2":  char2,
-		"3":  char3,
-		"4":  char4,
-		"5":  char5,
-		"6":  char6,
-		"7":  char7,
-		"8":  char8,
-		"9":  char9,
-		"10": char10,
-		"a":  a,
-		"j":  j,
-		"k":  k,
-		"q":  q,
-	}
-	keys := [...]string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "a", "j", "q", "k"}
-	if len(os.Args) >= 4 {
-		alignment := os.Args[1]
-		if alignment == "v" {
-			cards(keys, stringMap)
-			fmt.Println(stringMap[card1])
-			fmt.Println(stringMap[card2])
-		} else if alignment == "h" {
-			cards(keys, stringMap)
-			card1Arr := strings.Split(stringMap[card1], "\n")
-			card2Arr := strings.Split(stringMap[card2], "\n")
-			for i := range [11]int{} {
-				fmt.Println(card1Arr[i] + "  " + card2Arr[i])
-			}
-		} else {
-			fmt.Println("Invalid Positional Argument. Enter v for vertical/h for horizantal.")
-		}
-	} else {
-		fmt.Println("Too Few Commandline Arguments.")
-	}
-
+    stringMap := map[string]string{
+        "2":  char2,
+        "3":  char3,
+        "4":  char4,
+        "5":  char5,
+        "6":  char6,
+        "7":  char7,
+        "8":  char8,
+        "9":  char9,
+        "10": char10,
+        "a":  a,
+        "j":  j,
+        "k":  k,
+        "q":  q,
+    }
+    keys := [...]string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "a", "j", "q", "k"}
+    if len(os.Args) <=2{
+        fmt.Println("Too Few Arguments.")
+    }else{
+        alignment:=os.Args[1]
+        if alignment=="v"{
+            for i:=2;i<len(os.Args);i++{
+                if os.Args[i]=="r"{
+                    card:=keys[rand.Intn(len(keys))]
+                    fmt.Println(stringMap[card])
+                }else{
+                    _,keyFound:=stringMap[os.Args[i]]
+                    if keyFound{
+                        fmt.Println(stringMap[os.Args[i]])
+                    }else{
+                        fmt.Println("Invalid Commandline Argument. Choose from 2-10/a,k,q,j or r.\nTerminating the Program.")
+                        break
+                    }    
+                }
+            }
+        }else if alignment=="h"{
+            args:=os.Args[2:]
+            // cards:=make([][]string,len(args))
+            var cards [][]string
+            for _,v:=range args{
+                if v=="r"{
+                    cards=append(cards,strings.Split(stringMap[keys[rand.Intn(len(keys))]],"\n"))
+                }else{
+                    _,keyFound:=stringMap[v]
+                    if keyFound{
+                        cards=append(cards,strings.Split(stringMap[v],"\n"))
+                    }else{
+                        fmt.Println("Invalid Commandline Argument Found. Choose from 2-10/a,k,q,j or r.\nTerminating the code.")
+                        break
+                    }
+                }
+            }
+            for j:=range [11]int{}{
+                line:=""
+                for i:=range cards{
+                    line=line+" "+cards[i][j]
+                }
+                fmt.Println(line)
+            }
+            
+        }else{
+            fmt.Println("Invalid Positional Argument. Choose h for Horizantal/v for Vertical.")
+        }
+    }
 }
